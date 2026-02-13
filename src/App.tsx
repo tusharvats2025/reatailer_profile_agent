41bd547d7c177dc3832cc2ccc482d146f0d1e252
tusharvats2025 <tushar007vats@gmail.com>
2026-02-13 21:59:06 +0530

    Initial Vite + React + TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Retail Analytics Dashboard</h1>
+      <p>Project initialized successfully.</p>
+    </div>
+  );
+}
+
+export default App
